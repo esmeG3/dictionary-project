7c5c3771ce11b5ff632ed661104bad34671903b0
esG3 <esmeG3@users.noreply.github.com>
2024-04-23 15:23:03 +0100

    adding api  to project

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 
 export default function Dictionary() {
-let [keyword, setKeyword] = useState("")
+  let [keyword, setKeyword] = useState("");
 
-    function search(event) {
+  function handleResponse(response) {
+    console.log(response.data)
+  }
+
+  function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`)
-    }
+    let apiKey = `982865b6c020427a6364ef4b31o6dt0f`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);  
+}
+  // https://api.shecodes.io/dictionary/v1/define?word=sunset&key=982865b6c020427a6364ef4b31o6dt0f
 
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    return (
-        <div className="dictionary">
-            <form onSubmit={search}> 
-                <input type="search" placeholder="definition of.." onChange={handleKeywordChange} />
-                <input type="submit" value="Search"
-/>            </form>
-        </div>
-    )
+  return (
+    <div className="dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="definition of.."
+          onChange={handleKeywordChange}
+        />
+        <input type="submit" value="Search" />{" "}
+      </form>
+    </div>
+  );
 }
