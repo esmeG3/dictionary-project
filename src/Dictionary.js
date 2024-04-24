9b314cd456f26b760c56fa7380ff709c135f74b4
esG3 <esmeG3@users.noreply.github.com>
2024-04-24 09:45:26 +0100

    integrated the dictionary api for definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,7 +8,7 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    setResults=(response.data.meanings[0]);
+    setResults(response.data);
   }
 
   function search(event) {
---
+++ src/Meaning.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Meaning(props) { 
+
+    return (
+        <div className="Meaning">
+        <h4>
+            {props.meaning.partOfSpeech}
+            </h4>
+            <p>
+                {props.meaning.definition}
+            </p>
+            <p>
+                <em>{props.meaning.example}</em>
+            </p>
+        </div>
+     
+    )
+}
