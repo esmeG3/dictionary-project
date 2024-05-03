7ebb55ed36187b862bb58d67353dd1109265c650
esG3 <esmeG3@users.noreply.github.com>
2024-05-03 19:04:45 +0100

    stated integrating api for photo gallery

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePhotoResponse(response){
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiKey = `982865b6c020427a6364ef4b31o6dt0f`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);  
+
+    const apiKeyPhoto = `982865b6c020427a6364ef4b31o6dt0f`;
+    let apiUrlPhoto = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKeyPhoto}`;
+    axios.get(apiUrlPhoto).then(handlePhotoResponse);
   }
 
   function handleSubmit(event) {
@@ -45,11 +55,12 @@ if (loaded) {
           defaultValue={props.defaultKeyword}
         />
         
-        <button type="submit" class="btn">
-          <i class="fa-solid fa-magnifying-glass"></i>
+        <button type="submit" className="btn">
+          <i className="fa-solid fa-magnifying-glass"></i>
           </button>
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 } else {
